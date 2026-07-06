6d40344263eca2cccf086dae38907324ab327d0f
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-06 23:24:49 +0530

    feat: implement book routes

---
+++ routes/bookRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const validate = require('../middleware/validate');
+const bookSchema = require('../validators/bookValidator');
+
+const { addBook, getAllBooks, getBookById, updateBook, deleteBook, searchBookByTitle, filterBooksByGenre } = require('../controllers/bookController');
+
+router.post('/', validate(bookSchema), addBook);
+router.get('/search', searchBookByTitle);
+router.get('/filter', filterBooksByGenre);
+router.get('/', getAllBooks);
+router.get('/:id', getBookById);
+router.put('/:id', validate(bookSchema), updateBook);
+router.delete('/:id', deleteBook);
+
+module.exports = router;
